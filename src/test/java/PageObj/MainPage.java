e7c10af4f3c6b7ff4ed4e161b300ceb4c9fd3cb7
madmados <mados1489@gmail.com>
2022-08-10 12:53:42 +0300

    First Commit

--- src/test/java/PageObj/MainPage.java
+++ src/test/java/PageObj/MainPage.java
@@ -1,2 +1,24 @@
-package PageObj;public class MainPage {
+package PageObj;
+
+import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.SelenideElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.How;
+
+public class MainPage {
+    @FindBy(how = How.XPATH, using = "/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input")
+    private SelenideElement searchField;
+    @FindBy(how = How.CLASS_NAME, using = "gNO89b")
+    private SelenideElement searchButton;
+    @FindBy(how = How.XPATH, using = "//*[@id=\"hdtb-msb\"]/div[1]/div/div[4]/a")
+    private SelenideElement clickImages;
+    @FindBy(how = How.XPATH, using = "//*[@id=\"islrg\"]/div[1]/div[3]/a[1]/div[1]/img")
+    private SelenideElement clickImage;
+
+    public void takePic(String type) {
+        searchField.setValue(type);
+        searchButton.click();
+        clickImages.click();
+        clickImage.click();
+    }
 }
--- src/test/java/SimpleTest.java
+++ src/test/java/SimpleTest.java
@@ -1,13 +1,60 @@
-import org.junit.Before;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import PageObj.MainPage;
+import org.junit.Test;
 
-public class Test {
-    private static WebDriver driver;
+import java.io.BufferedInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
 
-    @Before
-    public void setUp() {
-        driver = new ChromeDriver();
-        driver.get("https://www.google.ru/");
+import static com.codeborne.selenide.Selectors.*;
+import static com.codeborne.selenide.Selenide.*;
+
+public class SimpleTest {
+    @Test
+    /* Тест не проходит из-за ошибки "unable to find valid certification path to requested target"
+    ошибка возникает при скачке конкретно 3 картинки.
+    Не уверен, входило ли это в ТЗ, но решил пока сдать так, т.к. как решить эту проблему не совсем знаю.
+    Если конкретно, то я не знаю и не понимаю, где должен лежать сертификат сайта
+     */
+    public void simpleTest() {
+        MainPage ObjPage =
+                open("https://www.google.ru/", MainPage.class);
+        ObjPage.takePic("Марьинский Александр");
+        String url = $(byXpath("//*[@id=\"Sva75c\"]/div/div/div[3]/div[2]/c-wiz/div/div[1]/div[1]/div[3]/div/a/img"))
+                .getAttribute("src");
+        String fileName = "src/test/java/PageObj/pic.jpg";
+
+        FileOutputStream fout = null;
+        BufferedInputStream in = null;
+
+        try {
+            in = new BufferedInputStream(new URL(url).openStream());
+            fout = new FileOutputStream(fileName);
+            byte data[] = new byte[1024];
+            int count;
+            while ((count = in.read(data, 0, 1024)) != -1) {
+                fout.write(data, 0, count);
+                fout.flush();
+            }
+        } catch (
+                MalformedURLException e) {
+            e.printStackTrace();
+        } catch (
+                IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                in.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                try {
+                    fout.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 }
